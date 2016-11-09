consultiacl/copuchalo

commit b892ec9d52b510c09c9180411fa1edb8a94266fc

Changed time between posts and minimal fixes

www/backend/post_edit.php
@@ -34,7 +34,7 @@ if (!empty($_REQUEST['user_id'])) {
 			$post->author=$current_user->user_id;
 			$post->print_edit_form();
 		} else {
-			echo 'Error: ' . _('debe esperar entre postits');
+			echo 'Error: ' . _('debe esperar entre postits') . ' (' . $globals['posts_period'] . ' segundos)');
 			die;
 		}
 	}
@@ -100,7 +100,7 @@ function save_post ($post_id) {
 
 		// Verify that there are a period of 1 minute between posts.
 		if(intval($db->get_var("select count(*) from posts where post_user_id = $current_user->user_id and post_date > date_sub(now(), interval ".$globals['posts_period']." second)"))> 0) {
-			echo 'ERROR: ' . _('debe esperar entre postits');
+			echo 'ERROR: ' . _('debe esperar entre postits') . ' (' . $globals['posts_period'] . ' segundos)');
 			die;
 		};
 

www/api/newpost.php
@@ -75,8 +75,8 @@ if($post->same_text_count(60) > 0) {
 };
 
 // Verify that there are a period of 1 minute between posts.
-if(intval($db->get_var("select count(*) from posts where post_user_id = $user->id and post_date > date_sub(now(), interval 1 minute)"))> 0) {
-		echo 'KO: ' . _('debe esperar 1 minuto entre postits');
+if(intval($db->get_var("select count(*) from posts where post_user_id = $user->id and post_date > date_sub(now(), interval 20 second)"))> 0) {
+		echo 'KO: ' . _('debe esperar 20 segundos entre postits');
 		die;
 };
 

commit 8292ea337f3b2c1a253a9b99937729c6f00923cf

Fix bug introduced in b892ec9

www/backend/post_edit.php
@@ -34,7 +34,7 @@ if (!empty($_REQUEST['user_id'])) {
 			$post->author=$current_user->user_id;
 			$post->print_edit_form();
 		} else {
-			echo 'Error: ' . _('debe esperar entre postits') . ' (' . $globals['posts_period'] . ' segundos)');
+			echo 'Error: ' . _('debe esperar entre postits') . ' (' . $globals['posts_period'] . ' segundos)';
 			die;
 		}
 	}
@@ -100,7 +100,7 @@ function save_post ($post_id) {
 
 		// Verify that there are a period of 1 minute between posts.
 		if(intval($db->get_var("select count(*) from posts where post_user_id = $current_user->user_id and post_date > date_sub(now(), interval ".$globals['posts_period']." second)"))> 0) {
-			echo 'ERROR: ' . _('debe esperar entre postits') . ' (' . $globals['posts_period'] . ' segundos)');
+			echo 'ERROR: ' . _('debe esperar entre postits') . ' (' . $globals['posts_period'] . ' segundos)';
 			die;
 		};
 