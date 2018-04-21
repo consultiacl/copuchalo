7b66528ece68e3233b1318e3212e06c00a86708d
Fermin Molina <fmolina@gmail.com>
2018-04-21 20:53:11 +0000

    Parametrized header bar color. Minor CSS fixes.

--- www/css/main.php
+++ www/css/main.php
@@ -3,10 +3,10 @@ include('../config.php');
 header('Content-Type: text/css; charset=utf-8');
 header('Cache-Control: public, max-age=864000');
 header("Expires: " . gmdate("r", $globals['now'] + 864000));
-header('Last-Modified: ' .  gmdate('D, d M Y H:i:s', max(filemtime('mediatize.css'), filemtime('handheld.css')) ) . ' GMT');
+header('Last-Modified: ' .  gmdate('D, d M Y H:i:s', max(filemtime('main.css'), filemtime('handheld.css')) ) . ' GMT');
 
 Haanga::Load('css/colorbox.css');
-Haanga::Load('css/mediatize.css');
+Haanga::Load('css/main.css');
 
 /* Include handheld classes for mobile/tablets */
 if ($globals['mobile']) {
