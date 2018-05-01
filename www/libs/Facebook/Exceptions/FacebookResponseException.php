c97c87b8a1840b91cb1c4c337b070d263ebf7f0f
Fermin Molina <fmolina@gmail.com>
2018-05-01 06:09:42 +0000

    Updated Facebook api to version 5.6.2

--- www/libs/Facebook/Exceptions/FacebookResponseException.php
+++ www/libs/Facebook/Exceptions/FacebookResponseException.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2016 Facebook, Inc.
+ * Copyright 2017 Facebook, Inc.
  *
  * You are hereby granted a non-exclusive, worldwide, royalty-free license to
  * use, copy, modify, and distribute this software in source code or binary
@@ -113,7 +113,9 @@ class FacebookResponseException extends FacebookSDKException
             // API Throttling
             case 4:
             case 17:
+            case 32:
             case 341:
+            case 613:
                 return new static($response, new FacebookThrottleException($message, $code));
 
             // Duplicate Post
